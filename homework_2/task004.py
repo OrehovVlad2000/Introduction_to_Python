79ce2e8944f4dcf9e8690d97fc43f12cf6fc6ffa
orehov_vlad <orehov.vlad21@gmsil.com>
2022-10-18 20:42:21 +0600

    add task 1 - 5

---
+++ homework_2/task004.py
@@ -0,0 +1,21 @@
+#Задайте список из N элементов, заполненных числами из промежутка [-N, N]. Найдите произведение элементов
+# на указанных позициях. Позиции хранятся в файле file.txt в одной строке одно число.
+
+import random
+
+num = int(input("Введите количество элементов списка: "))
+
+num_list = [random.randint(-num, num) for i in range(num)]
+
+position = open('file.txt', 'r')
+my_lines = position.readlines()
+count = 1
+
+for i in range(len(my_lines)):
+    pos = int(my_lines[i])
+    if pos < num:
+        count *= num_list[pos]
+    else:
+        continue
+position.close()
+print(count)
