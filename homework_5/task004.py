15783ff29b0f858eb1ea66bf1c32e1abcfb9f24f
Orehov Vlad <111432628+OrehovVlad2000@users.noreply.github.com>
2022-11-12 20:44:40 +0600

    Update task004.py

--- homework_5/task004.py
+++ homework_5/task004.py
@@ -2,7 +2,7 @@
 # Входные и выходные данные хранятся в отдельных текстовых файлах.
 
 with open('source_text.txt', 'w') as file:
-    file.write('AAABBFFFFIKKN')
+    file.write('AAAAAAAAAAAAAAA55 fffffff')
 
 with open('source_text.txt', 'r') as file:
     data = file.readline()
@@ -18,7 +18,13 @@ if not data:
 for each in data:
     if each != prev_elem:
         if prev_elem:
-            encoding += str(count) + prev_elem
+            if count > 9:
+                temp = count
+                while temp > 0:
+                    encoding += f'{9 if temp > 9 else temp}{prev_elem}'
+                    temp -= 9
+            else:
+                encoding += f'{count}{prev_elem}'
         count = 1
         prev_elem = each
     else:
@@ -32,13 +38,21 @@ with open('encoding.txt', 'r') as txt_encode:
     encoded_data = txt_encode.readline()
 decoding = ''
 counter = ''
+prev = ''
 
 for each in encoded_data:
     if each.isdigit():
-        counter += each
+        if prev.isdigit():
+            decoding += each * int(counter)
+            prev = ''
+            counter = ''
+        else:
+            counter += each
+            prev = each
     else:
         decoding += each * int(counter)
         counter = ''
+        prev = ''
 
 with open('decoding.txt', 'w') as txt_decode:
     txt_decode.write(decoding)
