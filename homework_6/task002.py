a2ccedc0cc1e97182b554d212d09f3fc72b61421
Orehov Vlad <111432628+OrehovVlad2000@users.noreply.github.com>
2022-11-16 20:06:09 +0600

    Update task002.py

--- homework_6/task002.py
+++ homework_6/task002.py
@@ -4,30 +4,8 @@
 # [1, 2, 3, 5, 1, 5, 3, 10] => [2, 10] и [1, 3, 5] и [1, 2, 5, 3, 10]
 
 num_list = [1, 2, 3, 5, 1, 5, 3, 10]
-print(num_list)
-sort_list = []
-sort_list_2 = []
 
-for i in range(len(num_list)):
-    count = 0
-    for j in range(len(num_list)):
-        if num_list[i] == num_list[j]:
-            count += 1
-    if count == 1:
-        sort_list.append(num_list[i])
-
-print(sort_list)
-
-for i in range(len(num_list)):
-    count = 0
-    for j in range(len(num_list)):
-        if num_list[i] == num_list[j]:
-            count += 1
-    if count > 1:
-        if num_list[i] not in sort_list_2:
-            sort_list_2.append(num_list[i])
-
-print(sort_list_2)
-
-num_list = list(set(num_list))
-print(num_list)
+print(f'Исходный список: {num_list}')
+print(f'Уникальные элементы: {[i for i in set(num_list) if num_list.count(i) == 1]}')
+print(f'Дубликаты: {[i for i in set(num_list) if num_list.count(i) > 1]}')
+print(f'Список без дубликатов: {list(set(num_list))}')
