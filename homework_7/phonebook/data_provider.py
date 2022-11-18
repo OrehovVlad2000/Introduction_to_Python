3ed2b9b8e1e37b1f31280ba3f12eb612f86d1c39
orehov_vlad <orehov.vlad21@gmsil.com>
2022-11-18 21:25:59 +0600

    add phonebook

---
+++ homework_7/phonebook/data_provider.py
@@ -0,0 +1,19 @@
+def get_name():
+    name = input("Введите имя: ")
+    return name
+
+def get_surname():
+    surname = input("Введите фамилию: ")
+    return surname
+
+def get_birthday():
+    birt = input("Введите дату рождения: ")
+    return birt
+
+def get_phone():
+    phone = input("Введите номер телефона: ")
+    return phone
+
+def get_action():
+    return int(input(' 0: Просмотр\n 1: Добавление\n 2: Выход\nВыберите действие:'))
+
